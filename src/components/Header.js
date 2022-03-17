a635bae2f288789a92480c2ad424a0b64f2a36da
Alexis Radice <alexis.radice.dev@gmail.com>
2022-03-17 20:30:16 +0100

    change created by developed

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <header>
       <h1>The Best French-Speaking Brawlhalla Players</h1>
-      Created by 
+      Developed by 
       <a href="https://twitter.com/AlexisBH_" target="_blank" rel="noopener noreferrer"> AlexisBH_</a>
     </header>
   );
