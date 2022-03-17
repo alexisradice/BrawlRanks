0f7418a1f9031f91c7e6b84167343a55ae06abd8
Alexis Radice <alexis.radice.dev@gmail.com>
2022-03-17 05:44:48 +0100

    change france to french speaking

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from "react";
 const Header = () => {
   return (
     <header>
-      <h1>Best France Brawlhalla Players</h1>
+      <h1>The Best French-Speaking Brawlhalla Players</h1>
     </header>
   );
 };
