e7020ced0ebb16a6101a6f14de2619fb9e4f8f1e
Alexis Radice <alexis.radice.dev@gmail.com>
2022-03-17 19:17:21 +0100

    add "Created by" <a>

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,12 @@
 import React from "react";
+// import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
     <header>
       <h1>The Best French-Speaking Brawlhalla Players</h1>
+      Created by 
+      <a href="https://twitter.com/AlexisBH_" target="_blank" rel="noopener noreferrer"> AlexisBH_</a>
     </header>
   );
 };
